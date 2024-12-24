29da4b3908f85eff637bee25ab4fc25b1da5c87c
mohitgujarati <mohitgujarati11@gmail.com>
2024-12-24 12:29:21 -0500

    inital push

---
+++ app/src/main/java/com/example/foodebug/BakingScreen.kt
@@ -0,0 +1,173 @@
+package com.example.foodebug
+
+import android.graphics.BitmapFactory
+import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+val images = arrayOf(
+    // Image generated using Gemini from the prompt "cupcake image"
+    R.drawable.baked_goods_1,
+    // Image generated using Gemini from the prompt "cookies images"
+    R.drawable.baked_goods_2,
+    // Image generated using Gemini from the prompt "cake images"
+    R.drawable.baked_goods_3,
+)
+val imageDescriptions = arrayOf(
+    R.string.image1_description,
+    R.string.image2_description,
+    R.string.image3_description,
+)
+
+@Preview(showBackground = true)
+@Composable
+fun BakingScreen(
+    bakingViewModel: BakingViewModel = viewModel()
+) {
+    val selectedImage = remember { mutableIntStateOf(0) }
+    val placeholderPrompt = stringResource(R.string.prompt_placeholder)
+    val placeholderResult = stringResource(R.string.results_placeholder)
+    var prompt by rememberSaveable { mutableStateOf(placeholderPrompt) }
+    var result by rememberSaveable { mutableStateOf(placeholderResult) }
+    val uiState by bakingViewModel.uiState.collectAsState()
+    val context = LocalContext.current
+    val scrollState = rememberScrollState()
+    var UsersThing= remember { mutableStateOf("") }
+    var UsersPreference= remember { mutableStateOf("") }
+    var UsersInstructions= remember { mutableStateOf("") }
+
+    var MainPromt = "Provide a recipe by using these things $UsersThing and taste preference $UsersPreference and instructions $UsersInstructions"
+
+    Column(
+        modifier = Modifier
+            .verticalScroll(scrollState)
+            .fillMaxSize()
+
+    ) {
+        Text(
+            text = stringResource(R.string.baking_title),
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = UsersThing.value,
+            label = { Text(stringResource(R.string.label_things_you_have)) },
+            onValueChange = { UsersThing.value = it },
+            modifier = Modifier
+                .height(100.dp)
+                .padding(10.dp)
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = UsersPreference.value,
+            label = { Text(stringResource(R.string.label_Enter_taste_preference)) },
+            onValueChange = { UsersPreference.value = it },
+            modifier = Modifier
+                .height(100.dp)
+                .padding(10.dp)
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+        )
+
+        Column(
+
+        ) {
+            TextField(
+                value = UsersInstructions.value,
+                label = { Text(stringResource(R.string.label_Intructions)) },
+                onValueChange = { UsersInstructions.value = it },
+                modifier = Modifier
+                    .height(100.dp)
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+            )
+
+            Button(
+
+                onClick = {
+                    val bitmap = BitmapFactory.decodeResource(
+                        context.resources,
+                        images[selectedImage.intValue]
+                    )
+                    bakingViewModel.sendPrompt(bitmap, MainPromt)
+                    Log.d("MainActivity", "Button clicked $MainPromt")
+                },
+                enabled = prompt.isNotEmpty(),
+                modifier = Modifier
+                    .padding(top = 10.dp)
+                    .width(200.dp)
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = stringResource(R.string.action_go))
+            }
+        }
+
+        if (uiState is UiState.Loading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+        } else {
+            var textColor = MaterialTheme.colorScheme.onSurface
+            if (uiState is UiState.Error) {
+                textColor = MaterialTheme.colorScheme.error
+                result = (uiState as UiState.Error).errorMessage
+            } else if (uiState is UiState.Success) {
+                textColor = MaterialTheme.colorScheme.onSurface
+                result = (uiState as UiState.Success).outputText
+            }
+
+            Text(
+                text = result,
+                textAlign = TextAlign.Start,
+                color = textColor,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp)
+                    .fillMaxSize()
+
+            )
+        }
+    }
+}
+
+
